487007a83efa836c658ebee249f3a6bf8dd97493
allicn03 <a.neusch@gmail.com>
2017-01-23 10:37:50 -0500

    Add files via upload

---
+++ cf_template-1.1.1/js/scripts.js
@@ -0,0 +1,36 @@
+// Twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)
+	[0],p=/^http:/.test(d.location)?'http':'https';if
+	(!d.getElementById(id)){js=d.createElement(s);
+		js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+		fjs.parentNode.insertBefore(js,fjs);}}
+		(document, 'script', 'twitter-wjs');
+
+
+$(document).ready(function(){
+
+  	//smooth scrolling
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+      var href = $.attr(this, 'href');
+      $root.animate({
+          scrollTop: $(href).offset().top
+      }, 500, function () {
+          window.location.hash = href;
+      });
+      return false;
+    });
+  $('#submit').on('click', function() {
+  	var comment = $('#message-box').val();
+  	console.log(comment);
+  	$('#visible-comment').html(comment);
+  	$('#message-box').hide();
+  	$('#submit').hide();
+  	$('#received').append("We have received your message!");
+  	return false;
+  });
+
+});
+	
+   
+
