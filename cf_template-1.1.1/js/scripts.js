8385409f28f558e69139f7bd4d34b290a174adfa
allicn03 <a.neusch@gmail.com>
2017-01-28 11:40:52 -0500

    Add files via upload

---
+++ cf_template-1.1.1/js/scripts.js
@@ -0,0 +1,56 @@
+// Twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)
+	[0],p=/^http:/.test(d.location)?'http':'https';if
+	(!d.getElementById(id)){js=d.createElement(s);
+		js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+		fjs.parentNode.insertBefore(js,fjs);}}
+		(document, 'script', 'twitter-wjs');
+
+
+$(document).ready(function() {
+  	//smooth scrolling
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+      var href = $.attr(this, 'href');
+      $root.animate({
+          scrollTop: $(href).offset().top
+      }, 500, function () {
+          window.location.hash = href;
+      });
+      return false;
+    });
+  $('#submit').on('click', function() {
+  	var comment = $('#message-box').val();
+  	console.log(comment);
+  	$('#visible-comment').html(comment);
+  	if(comment === "") {
+  		$("#message-box").css("border", "2px solid red");
+  	} else {
+  		$('#message-box').hide();
+  	};
+  	$('#submit').hide();
+  	$('#received').append("We have received your message!");
+  	return false;
+  });
+  $('#message-box').on('keyup', function() {
+  	var charCount = $('#message-box').val().length;
+  	$('#char-count').html(charCount);
+  	if(charCount > 50) {
+  		$('#char-count').css("color", "red");
+		} else {
+		};
+
+  });
+
+  //work section
+  for(var i = 0; i < works.length; ++i) {
+  	$("#work-example").append("\
+  		<div class='col-md-3 col-xs-6'>\
+  			img class='img-responsive' src='" + works[i] + "'>\
+  		</div>\
+  	");
+  };
+});
+	
+   
+
---
+++ cf_template-1.1.1/js/work.js
@@ -0,0 +1,4 @@
+var works = [src = "img/pupno1adj3.jpg",
+  "http://www.animal-photography.com/thumbs/red_tabby_long_hair_kitten_~AP-0UJFTC-TH.jpg",
+  "http://www.animal-photography.com/thumbs/silver_tabby_kittens~AP-0JO6Y9-TH.jpg",
+  "http://www.animal-photography.com/thumbs/silver_tabby_kitten_looking_up~AP-0DLVMB-TH.jpg"]
