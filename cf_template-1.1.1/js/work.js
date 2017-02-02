aed7a2b132653bd743d394f512d35d9e7dbf196f
allicn03 <a.neusch@gmail.com>
2017-02-02 18:34:51 -0500

    Add files via upload
    this is my submission with google maps and markers.

---
+++ cf_template-1.1.1/js/work.js
@@ -0,0 +1,41 @@
+var works = [
+	{
+		title: "First Project",
+		pic: "img/pupno1adj3.jpg",
+		url: "https://www.google.com/"
+	},
+	{
+		title: "Second Project",
+		pic: "img/pupno1adj3.jpg",
+		url: "https://www.google.com/"	
+	},
+	{
+		title: "Third Project",
+		pic: "img/pupno1adj3.jpg",
+		url: "https://www.google.com/"
+	},
+	{
+		title: "Fourth Project",
+		pic: "img/pupno1adj3.jpg",
+		url: "https://www.google.com/"
+	},
+	{
+		title: "Fifth Project",
+		pic: "img/pupno1adj3.jpg",
+		url: "https://www.google.com/"
+	},
+	{
+		title: "Sixth Project",
+		pic: "img/pupno1adj3.jpg",
+		url: "https://www.google.com/"
+	},
+	{
+		title: "Seventh Project",
+		pic: "img/pupno1adj3.jpg",
+		url: "https://www.google.com/"
+	},
+	{
+		title: "Eighth Project",
+		pic: "img/pupno1adj3.jpg",
+		url: "https://www.google.com/"
+	}]
